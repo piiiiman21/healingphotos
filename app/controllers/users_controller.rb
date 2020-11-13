67da17df3c80e1ef4ddaf3b8963d01f244f4c049
Piiiiman21 <piiiiman21good@gmail.com>
2020-11-13 14:10:08 +0000

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :likes] 
+  before_action :require_user_logged_in, only: [:index, :show, :likes, :edit, :update, :destroy] 
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
 
   def create
     @user = User.new(user_params)
@@ -27,6 +28,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'ユーザー情報が更新されました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザーは更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'ユーザーは正常に削除されました'
+    redirect_to users_url
+  end
+  
   def likes
     @user = User.find(params[:id])
     @likes = @user.likes.page(params[:page])
@@ -35,6 +58,7 @@ class UsersController < ApplicationController
   
   private
 
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     member do
       get :likes
     end
